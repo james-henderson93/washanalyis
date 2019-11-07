582034c50b0a78c649e17acb89821a59446fab7e
james-henderson93 <53478465+james-henderson93@users.noreply.github.com>
2019-11-07 17:11:45 +0300

    Add files via upload

---
+++ load_inputs.R
@@ -0,0 +1,39 @@
+
+# questionnaire
+questions <- read.csv("input/kobo_questions.csv", 
+                      stringsAsFactors=F, check.names=F)
+
+
+choices <- read.csv("input/kobo_choices.csv", 
+                    stringsAsFactors=F, check.names=F)
+
+# sampling
+
+cluster_lookup_table <- read.csv("input/combined_sample_ids.csv", 
+                                 stringsAsFactors=F, check.names=F)
+lookup_table <- read.csv("input/lookup_table_names.csv", stringsAsFactors = F)
+
+
+samplingframe <- load_samplingframe("./input/Strata_clusters_population.csv")
+
+# samplingframe_in_camp<-load_samplingframe("./input/sampling_frame_in_camp.csv")
+
+
+# data
+response <- read.csv("input/data_cleaned_anonymised.csv",
+                     stringsAsFactors = F, check.names = F)
+
+
+# idp_in_camp <- read.csv("input/idp_in_camp.csv",
+                        # stringsAsFactors = F, check.names = F)
+# idp_in_camp$district <- to_alphanumeric_lowercase(samplingframe_in_camp$district.new[match(idp_in_camp$camp, samplingframe_in_camp$camp)])
+
+
+
+
+# simple_random_strata[,1]<-to_alphanumeric_lowercase(simple_random_strata[,1])
+# simple_random_strata<-paste0(simple_random_strata[,1],simple_random_strata[,2])
+
+
+
+
